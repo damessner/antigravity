de2c1e48bf83d10dcd046f73de242e8e8ff81f89
David Messner <da.messner@tsn.at>
2026-05-14 16:44:21 +0200

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -8,6 +8,13 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    allowedDevOrigins: [
+      "http://localhost:3000",
+      "http://192.168.178.191:3000",
+      "http://192.168.1.100:3000",
+    ],
+  },
 };
 
 export default nextConfig;
